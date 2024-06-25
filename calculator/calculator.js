e9ca4c7ced091541ebfa66b081e2b71db0a2293d
Vishakha Mourya <vishakhamourya508@gmail.com>
2024-06-25 13:45:06 +0530

    calculator files added

---
+++ calculator/calculator.js
@@ -0,0 +1,38 @@
+//variable to store value
+let displayValue='';
+
+//function to input value
+function inputValue(value){
+    //values will be added to the display value variable
+    displayValue+=value;
+    //it update the display window
+    updateDisplay();
+}
+
+//function to clear the display window
+function clearDisplay(){
+    //it clear the display value variable
+    displayValue='';
+    //updates the display window
+    updateDisplay();
+}
+
+//function to update the display window
+function updateDisplay(){
+    //it update the display window
+    document.getElementById('display').innerText=displayValue||'00';
+}
+
+//function to calculate the result
+function calculateResult(){
+    try{
+        //it calculate the result
+        displayValue=eval(displayValue).toString();
+    }
+    catch(err){
+        //if error occurs
+        displayValue='Error';
+    }
+    //updates the display window
+    updateDisplay();
+}
